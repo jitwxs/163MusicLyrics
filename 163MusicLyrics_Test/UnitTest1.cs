3f247c1d5db2e7fe241c288d7d45ed5ec090c832
millennium <1543255825@qq.com>
2022-03-31 09:18:09 +0800

    添加依赖 #52

---
+++ 163MusicLyrics_Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace _163MusicLyrics_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
