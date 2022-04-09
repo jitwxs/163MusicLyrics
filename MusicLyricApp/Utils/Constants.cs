0bcaeb3dc2e531f1c7f9a2b032451546e63617de
jitwxs <jitwxs@foxmail.com>
2022-04-09 20:52:29 +0800

    add Constants.cs

---
+++ MusicLyricApp/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MusicLyricApp.Utils
+{
+    public static class Constants
+    {
+        public const string Version = "v4.1";
+        
+        public static readonly string SettingPath = Environment.CurrentDirectory + "\\setting.json";
+
+        public const int SettingFormOffset = 20;
+    }
+}
