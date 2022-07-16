06db71b21218d4225841455c88a36ae2043f5d67
jitwxs <jitwxs@foxmail.com>
2022-07-16 16:40:10 +0800

    upgrade v4.7

--- MusicLyricApp/Bean/Constants.cs
+++ MusicLyricApp/Bean/Constants.cs
@@ -4,7 +4,7 @@ namespace MusicLyricApp.Bean
 {
     public static class Constants
     {
-        public const string Version = "v4.6";
+        public const string Version = "v4.7";
         
         public static readonly string SettingPath = Environment.CurrentDirectory + "\\MusicLyricAppSetting.json";
         
