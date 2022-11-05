08cf3efcc0e552309d96fc5bb91e7b27910b93c9
jitwxs <jitwxs@foxmail.com>
2022-11-05 14:48:07 +0800

    upgrade version 5.2

--- MusicLyricApp/Bean/Constants.cs
+++ MusicLyricApp/Bean/Constants.cs
@@ -4,7 +4,7 @@ namespace MusicLyricApp.Bean
 {
     public static class Constants
     {
-        public const string Version = "v5.1";
+        public const string Version = "v5.2";
         
         public static readonly string SettingPath = Environment.CurrentDirectory + "\\MusicLyricAppSetting.json";
         
