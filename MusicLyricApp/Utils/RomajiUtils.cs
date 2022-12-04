67bf607c6292351e29b33ae1921f290c79eb581e
jitwxs <jitwxs@foxmail.com>
2022-12-04 17:16:04 +0800

    fix dotType bugs in verbatim lyric

--- MusicLyricApp/Utils/RomajiUtils.cs
+++ MusicLyricApp/Utils/RomajiUtils.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Kawazu;
 using MusicLyricApp.Bean;
@@ -11,11 +11,8 @@ namespace MusicLyricApp.Utils
     public static class RomajiUtils
     {
         public static async Task<List<LyricLineVo>> ToRomaji(List<LyricLineVo> inputList, List<LyricLineVo> faultList,
-            RomajiConfigBean romajiConfig, SettingBean settingBean)
+            RomajiConfigBean romajiConfig)
         {
-            var timestampFormat = settingBean.Param.LrcTimestampFormat;
-            var dotType = settingBean.Param.DotType;
-            
             if (inputList.Any(vo => Utilities.HasKana(vo.Content)))
             {
                 var converter = new KawazuConverter();
@@ -26,16 +23,13 @@ namespace MusicLyricApp.Utils
 
                 foreach (var vo in inputList)
                 {
-                    // need try split sub lyricLineVO, resolve verbatim lyric mode
-                    var content = new StringBuilder();
-                    foreach (var subVo in LyricLineVo.Split(vo))
-                    {
-                        var subContent = await converter.Convert(subVo.Content, To.Romaji, mode, system, "(", ")");
-                        
-                        content.Append(subVo.Timestamp.PrintTimestamp(timestampFormat, dotType) + subContent);
-                    }
+                    var content = await converter.Convert(vo.Content, To.Romaji, mode, system, "(", ")");
                     
-                    resultList.Add(new LyricLineVo(content.ToString()));
+                    // 去重时间戳中的空格 eg [ 00 : 23 . 113 ]
+                    content = Regex.Replace(content, "\\[\\s+\\d+\\s+:\\s+\\d+\\s+\\.\\s+\\d+\\s+]", 
+                        m => m.ToString().Replace(" ", ""));
+                    
+                    resultList.Add(new LyricLineVo(content, vo.Timestamp));
                 }
 
                 return resultList;
