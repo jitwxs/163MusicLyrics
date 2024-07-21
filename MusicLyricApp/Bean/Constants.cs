4c22aa11956bebcd6800487bea5a293ce114c906
jitwxs <jitwxs@foxmail.com>
2024-07-21 11:47:18 +0800

    initial v6.3 version

--- MusicLyricApp/Bean/Constants.cs
+++ MusicLyricApp/Bean/Constants.cs
@@ -5,7 +5,7 @@ namespace MusicLyricApp.Bean
 {
     public static class Constants
     {
-        public const string Version = "v6.2";
+        public const string Version = "v6.3";
         
         public static readonly string SettingPath = Environment.CurrentDirectory + "\\MusicLyricAppSetting.json";
         
